9ef18de934fcf0e3bbc84c7972184afe6727fcfc
Anshul Badoni <91309365+AnshulBadoni@users.noreply.github.com>
2025-11-12 10:22:51 +0530

    Update page.tsx

--- src/app/(root)/companies/[id]/page.tsx
+++ src/app/(root)/companies/[id]/page.tsx
@@ -63,7 +63,6 @@ export default function CompanyDashboardPage() {
             {/* Banner */}
             <div className="relative h-40">
                 <img src="https://w.waj81.jpg" className="w-full h-full rounded-2xl object-cover opacity-90" />
-                {/* <video src="https://el.phncdn.com/pics/gifs/002/984/951/2984951a.webm" autoPlay loop muted className="w-full h-full rounded-2xl object-cover"></video> */}
                 <div className="absolute bottom-0 left-44 -translate-x-1/2 transform">
                     <img src={companyData.logo} alt={companyData.name} className="sm:w-32 w-24 object-contain rounded-full border-4 border-white dark:border-neutral-900 shadow-lg -mb-14 bg-white dark:bg-neutral-900 p-3" />
                 </div>
