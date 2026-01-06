69e35c7c692fe52d657c7cc2644f9d8bd377da18
AnshulBadoni <anshulbadoni359@gmail.com>
2026-01-06 17:43:26 +0530

    Add logging for JWT token in dashboard page

--- src/app/(root)/dashboard/page.tsx
+++ src/app/(root)/dashboard/page.tsx
@@ -5,23 +5,22 @@ import Seeker from "@/app/components/dashboard/Seeker";
 import { User } from "@/app/types";
 
 export default async function Page() {
-    const cookieStore = cookies();
-    const token = (await cookieStore).get("jwt")?.value as string;
-    let user;
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET!);
-        console.log(decoded);
-        user = decoded as User;
-    } catch (err) {
-        console.error("JWT ERROR:", err);
-        throw err;
-    }
+    // const cookieStore = cookies();
+    // const token = (await cookieStore).get("jwt")?.value as string;
+    // console.log("token:",token)
+    // let user;
+    // try {
+    //     user = jwt.verify(token, process.env.JWT_SECRET as string) as User;
+    // } catch (err) {
+    //     return <p>Invalid or expired token</p>;
+    // }
 
     return (
         <>
-            {user.role.toLowerCase() == "seeker" && <Seeker />}
-            {user.role.toLowerCase() == "poster" && <Poster />}
-            {user.role.toLowerCase() == "admin" && <div>Admin Dashboard</div>}
+            {/* {user.role.toLowerCase() == "seeker" && <Seeker />} */}
+            {/* {user.role.toLowerCase() == "poster" && <Poster />} */}
+            {/* {user.role.toLowerCase() == "admin" && <div>Admin Dashboard</div>} */}
+            <Seeker />
         </>
     );
 }
